0a06dedb1c26f87f468c333c277985f318f51f4b
NxxhL <89213023+chifouu65@users.noreply.github.com>
2022-09-01 05:15:28 +0200

    add remove card shopping +1 / -1 and remove
    fix cart page
    
    price calc
    
    Need adding Stripe payement and succes page
    
    //avis , note product /

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,10 +2,12 @@ import {client, urlFor} from "../../lib/client";
 import {AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar} from "react-icons/ai";
 import {Product} from "../../components";
 import React, {useState} from 'react'
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({product, products}) => {
     const {image, name, details, price} = product;
     const [index, setIndex] = useState(0);
+    const { decQty, incQty, qty, onAdd } = useStateContext();
 
     return (
         <div>
@@ -48,13 +50,13 @@ const ProductDetails = ({product, products}) => {
                     <div className='quantity'>
                         <h3>Quantity:</h3>
                         <p className='quantity-desc'>
-                            <span className='minus' onClick=''><AiOutlineMinus/></span>
-                            <span className='num' onClick=''>0</span>
-                            <span className='plus' onClick=''><AiOutlinePlus/></span>
+                            <span className='minus' onClick={decQty}><AiOutlineMinus/></span>
+                            <span className='num' onClick=''>{qty}</span>
+                            <span className='plus' onClick={incQty}><AiOutlinePlus/></span>
                         </p>
                     </div>
                     <div className='buttons'>
-                        <button type="button" className='add-to-cart' onClick=''>Add to Card</button>
+                        <button type="button" className='add-to-cart' onClick={() => onAdd(product, qty)}>Add to Card</button>
                         <button type="button" className='buy-now' onClick=''>Shop Now</button>
                     </div>
                 </div>
